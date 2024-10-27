c2bd3c465cbb106c4db10f005cbd362cc4cee70c
hans <hkmelaa@hotmail.com>
2024-10-27 19:49:24 +0100

    insigths test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
 <script>
 	import '../global.css';
+
 	import Snowfall from '$lib/components/snow/Snowfall.svelte';
 	import Tree from '$lib/components/Tree.svelte';
-	import Music from '$lib/components/Music.svelte';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
 
 <Snowfall />
 <Tree />
-<!-- <Music />  -->
 
 <main>
 	{@render children?.()}
