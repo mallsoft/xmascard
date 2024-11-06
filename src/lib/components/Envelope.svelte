bcd233c74538afa3c03b46ec48e3caf03865db45
Celine <celine.habbestad.seland@dnb.no>
2024-11-06 16:00:13 +0100

    envelope first edition

---
+++ src/lib/components/Envelope.svelte
@@ -0,0 +1,56 @@
+<script>
+	// @ts-nocheck
+
+	let { children } = $props();
+	let showCard = $state(false);
+</script>
+
+<article class:showCard>
+	<button
+		class="flap"
+		onclick={() => (showCard = true)}
+		aria-label="open envelope flap"
+	>
+	</button>
+	<div>
+		{@render children()}
+	</div>
+</article>
+
+<style>
+	article {
+		background-color: rgb(185, 64, 64);
+		position: relative;
+		overflow: visible;
+	}
+
+	button {
+		appearance: none;
+		display: block;
+		position: absolute;
+		top: 0;
+
+		width: 100%;
+		height: 100%;
+
+		background-color: gainsboro;
+
+		clip-path: polygon(50% 40%, 0 0, 100% 0);
+
+		transform-origin: center top;
+		transition: transform 0.5s;
+	}
+
+	div {
+		opacity: 0;
+		transition: 0.5s;
+	}
+
+	.showCard div {
+		opacity: 1;
+	}
+
+	.showCard button {
+		transform: rotateX(-180deg);
+	}
+</style>
--- src/routes/(xmascard)/[card=cardParam]/+page.svelte
+++ src/routes/(xmascard)/[card=cardParam]/+page.svelte
@@ -1,6 +1,26 @@
-<script> 
-	export let data
+<script>
+	import Envelope from '$lib/components/Envelope.svelte';
+	export let data;
 </script>
 
-<h1>{data.title}</h1>
-<p>{data.message}</p>
+<div class="container">
+	<Envelope>
+		<h1>{data.title}</h1>
+		<p>{data.message}</p>
+	</Envelope>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+		text-align: center;
+	}
+
+	Envelope {
+		max-width: 400px;
+		padding: 200px;
+	}
+</style>
